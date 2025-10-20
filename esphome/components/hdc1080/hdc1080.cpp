996e060ff74a273c461c7c99cc3c66bfc4557e68
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-10-20 10:25:57 +1000

    Update hdc1080.cpp

--- esphome/components/hdc1080/hdc1080.cpp
+++ esphome/components/hdc1080/hdc1080.cpp
@@ -18,15 +18,19 @@ void HDC1080Component::setup() {
     // get boot config
     const uint8_t config[2] = {0x10,0x00};
     write_register(HDC1080_CMD_CONFIGURATION, config, 2);
-    if (this->write(&HDC1080_CMD_CONFIGURATION, 1) != i2c::ERROR_OK) {
+    if (read_register(HDC1080_CMD_CONFIGURATION, this->boot_config_, 2) != i2c::ERROR_OK) {
       this->status_set_warning();
-      ESP_LOGW(TAG, "Error writing config register");
-    } else {
-      if (this->read(this->boot_config_, 2) != i2c::ERROR_OK) {
-        this->status_set_warning();
-        ESP_LOGW(TAG, "Error reading configuration");
-      }
+      ESP_LOGW(TAG, "Error reading configuration");
     }
+    // if (this->write(&HDC1080_CMD_CONFIGURATION, 1) != i2c::ERROR_OK) {
+    //   this->status_set_warning();
+    //   ESP_LOGW(TAG, "Error writing config register");
+    // } else {
+    //   if (this->read(this->boot_config_, 2) != i2c::ERROR_OK) {
+    //     this->status_set_warning();
+    //     ESP_LOGW(TAG, "Error reading configuration");
+    //   }
+    // }
     this->setup_complete_= true;
     return;
   });
