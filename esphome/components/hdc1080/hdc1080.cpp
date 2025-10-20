d3dfa3d0aa2cb6d9648a2b4bff824d7d97aa1cdc
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-10-20 15:05:15 +1000

    no config

--- esphome/components/hdc1080/hdc1080.cpp
+++ esphome/components/hdc1080/hdc1080.cpp
@@ -12,25 +12,13 @@ static const uint8_t HDC1080_CMD_TEMPERATURE = 0x00;
 static const uint8_t HDC1080_CMD_HUMIDITY = 0x01;
 
 void HDC1080Component::setup() {
-  
   // delay after boot
   this->set_timeout(20, [this]() {
     // get boot config
-    //const uint8_t config[2] = {0x10,0x00};
-    //write_register(HDC1080_CMD_CONFIGURATION, config, 2);
     if (read_register(HDC1080_CMD_CONFIGURATION, this->boot_config_, 2) != i2c::ERROR_OK) {
       this->status_set_warning();
-      ESP_LOGW(TAG, "Error reading configuration");
+      this->setup_ok_= false;
     }
-    // if (this->write(&HDC1080_CMD_CONFIGURATION, 1) != i2c::ERROR_OK) {
-    //   this->status_set_warning();
-    //   ESP_LOGW(TAG, "Error writing config register");
-    // } else {
-    //   if (this->read(this->boot_config_, 2) != i2c::ERROR_OK) {
-    //     this->status_set_warning();
-    //     ESP_LOGW(TAG, "Error reading configuration");
-    //   }
-    // }
     this->setup_complete_= true;
     return;
   });
@@ -38,22 +26,23 @@ void HDC1080Component::setup() {
 
 void HDC1080Component::dump_config() {
   ESP_LOGCONFIG(TAG, "HDC1080:");
-  //ESP_LOGCONFIG(TAG, "  Config: 0x%04X", this->boot_config_);
-  ESP_LOGCONFIG(TAG, "  Configuration: 0x%02X%02X", this->boot_config_[0],this->boot_config_[1]);
-  LOG_I2C_DEVICE(this);
-  if (this->is_failed()) {
-    ESP_LOGE(TAG, ESP_LOG_MSG_COMM_FAIL);
+  if (this->setup_ok_) {
+    ESP_LOGCONFIG(TAG, "  Configuration: 0x%02X%02X", this->boot_config_[0],this->boot_config_[1]);
+  } else {
+    ESP_LOGW(TAG, "  Setup warning: error reading configuration");
   }
+  LOG_I2C_DEVICE(this);
   LOG_UPDATE_INTERVAL(this);
   LOG_SENSOR("  ", "Temperature", this->temperature_);
   LOG_SENSOR("  ", "Humidity", this->humidity_);
 }
 
 void HDC1080Component::update() {
-  // regardless of what sensor/s are defined in yaml configuration
-  // the hdc1080 setup configuration used, requires both temperature and humidity to be read
   if(!this->setup_complete_) return;
 
+  // regardless of what sensor/s are defined in yaml configuration
+  // the hdc1080 setup configuration used, requires both temperature and humidity to be read
+  
   this->status_clear_warning();
 
   if (this->write(&HDC1080_CMD_TEMPERATURE, 1) != i2c::ERROR_OK) {
--- esphome/components/hdc1080/hdc1080.h
+++ esphome/components/hdc1080/hdc1080.h
@@ -20,8 +20,10 @@ class HDC1080Component : public PollingComponent, public i2c::I2CDevice {
 
  protected:
   bool setup_complete_{false};
-  //uint16_t boot_config_{0xFFFF};
+  bool setup_ok_{true};
+  
   uint8_t boot_config_[2]{0xFF,0xFF};
+
   sensor::Sensor *temperature_{nullptr};
   sensor::Sensor *humidity_{nullptr};
 };
